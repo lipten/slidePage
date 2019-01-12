53d0797b69d809af8c2e364b7e55e91663fdbc4b
lipten <lipten@liptendeMacBook-Pro.local>
2019-01-12 16:51:10 +0800

    feature: add `dragMode` to slide page with drag gesture

--- slidePage.js
+++ slidePage.js
@@ -114,29 +114,68 @@ if (!("classList" in document.documentElement)) {
 			}
 		},
 		touchStart: function (e) {
-			touchPoint.startpoint = e.targetTouches[0].clientY;
+			if (!this.canSlide) {
+				touchPoint.startpoint = -1;
+			} else {
+				touchPoint.startpoint = e.targetTouches[0].clientY;
+			}
 		},
 		touchMove: function (e) {
+			if (touchPoint.startpoint === -1) {
+				e.preventDefault();
+				return false;
+			}
 			var offsetY = e.targetTouches[0].clientY - touchPoint.startpoint;
 			!this.canPrev && offsetY > 5 && (this.isScroll = true);    //-- 滚动到底部往上滑可继续滚动条滚动
 			!this.canNext && offsetY < -5 && (this.isScroll = true);   //-- 滚动到顶部往下滑可继续滚动条滚动
 			!this.isScroll && e.preventDefault();
-			touchPoint.endpoint = e.targetTouches[0].clientY;
+			if (this.opt.dragMode && this.canSlide && this.isScroll != true) {
+				if (offsetY < -5 && this.count > this.page && this.canNext) {
+					this.items[this.page-1].style.transform = 'translate3d(0, ' + offsetY.toFixed(2) + 'px, 0)';
+					this.items[this.page].style.transform = 'translate3d(0, ' + (window.innerHeight + offsetY).toFixed(2) + 'px, 0)';
+					touchPoint.endpoint = e.targetTouches[0].clientY;
+				}
+				if (offsetY > 5 && this.page > 1 && this.canPrev) {
+					this.items[this.page - 2].style.transform = 'translate3d(0, -' + (window.innerHeight - offsetY).toFixed(2) + 'px, 0)';
+					this.items[this.page - 1].style.transform = 'translate3d(0, ' + offsetY.toFixed(2) + 'px, 0)';
+					touchPoint.endpoint = e.targetTouches[0].clientY;
+				}
+			}
+			if (!this.opt.dragMode) {
+				touchPoint.endpoint = e.targetTouches[0].clientY;
+			}
 		},
 		touchEnd: function (e) {
-			if (touchPoint.endpoint === 0) {
+			if (touchPoint.endpoint === 0 || touchPoint.startpoint === -1) {
 				return false;
 			}
-			if ((touchPoint.endpoint - touchPoint.startpoint) < -60) {
-				this.canSlide && this.canNext && this.slideNext();
-			} else if ((touchPoint.endpoint - touchPoint.startpoint) > 60) {
-				this.canSlide && this.canPrev && this.slidePrev();
+			var offsetDrag = (touchPoint.endpoint - touchPoint.startpoint)
+			if (this.opt.dragMode) {
+				var thresholdDistance = window.innerHeight / 4;
+				if (offsetDrag < -1 * thresholdDistance) {
+					this.canSlide && this.canNext && this.slideNext();
+				} else if (offsetDrag > thresholdDistance) {
+					this.canSlide && this.canPrev && this.slidePrev();
+				} else if (offsetDrag > -1 * thresholdDistance && offsetDrag < thresholdDistance) {
+					methods.resetSlideForDrag.call(this);
+				}
+			} else {
+				if ((touchPoint.endpoint - touchPoint.startpoint) < -60) {
+					this.canSlide && this.canNext && this.slideNext();
+				} else if ((touchPoint.endpoint - touchPoint.startpoint) > 60) {
+					this.canSlide && this.canPrev && this.slidePrev();
+				}
 			}
 			touchPoint.startpoint = 0;
 			touchPoint.endpoint = 0;
 		},
 		transitionEnd: function (event) {
 			if (event.target.isEqualNode(this.items[this.page - 1])) {
+				if (this.opt.dragMode) {
+					this.items[this.page - 1].classList.remove('transition');
+					this.items[this.page - 2] && this.items[this.page - 2].classList.remove('transition');
+					this.items[this.page] && this.items[this.page].classList.remove('transition');
+				}
 				if (this.direction == 'next') {
 					this.opt.after(this.page - 1, this.direction, this.page);
 					this.opt.refresh && methods.resetAnimation.call(this, this.page - 2);
@@ -175,6 +214,19 @@ if (!("classList" in document.documentElement)) {
 				this.canPrev = true;
 				this.canNext = true;
 				this.isScroll = false;
+			}
+		},
+		resetSlideForDrag: function() {
+			this.canSlide = false;
+			this.items[this.page - 1].classList.add('transition');
+			this.items[this.page - 1].style.transform = 'translate3d(0, 0, 0)';
+			if (this.items[this.page - 2]) {
+				this.items[this.page - 2].classList.add('transition');
+				this.items[this.page - 2].style.transform = 'translate3d(0, -100%, 0)';
+			}
+			if (this.items[this.page]) {
+				this.items[this.page].classList.add('transition');
+				this.items[this.page].style.transform = 'translate3d(0, 100%, 0)';
 			}
 		},
 		// 重置动画状态，全部隐藏
@@ -246,12 +298,14 @@ if (!("classList" in document.documentElement)) {
 						item.style.transform = 'translate3d(0, 100%, 0)';
 					}
 				}
-				(function (item) {
-					var timer = setTimeout(function () {
-						if (item.classList && item.classList.add instanceof Function) item.classList.add('transition');
-						clearTimeout(timer);
-					});
-				})(item)
+				if (!this.opt.dragMode) {
+					(function (item) {
+						var timer = setTimeout(function () {
+							if (item.classList && item.classList.add instanceof Function) item.classList.add('transition');
+							clearTimeout(timer);
+						});
+					})(item)
+				}
 			}
 		},
 
@@ -291,6 +345,7 @@ if (!("classList" in document.documentElement)) {
 			useWheel: true,
 			useSwipe: true,
 			useAnimation: true,
+			dragMode: false,
 		};
 		this.canSlide = true;
 		this.canNext = true;
@@ -318,6 +373,10 @@ if (!("classList" in document.documentElement)) {
 		if (this.count <= this.page) {
 			return false;
 		}
+		if (this.opt.dragMode) {
+			this.items[this.page - 1].classList.add('transition');
+			this.items[this.page].classList.add('transition');
+		}
 		this.direction = 'next';
 		methods.slideScroll.call(this, this.page);
 		this.items[this.page - 1].style.transform = 'translate3d(0, -100%, 0)';
@@ -333,6 +392,10 @@ if (!("classList" in document.documentElement)) {
 	slidePage.prototype.slidePrev = function (optimize) {
 		if (1 >= this.page) {
 			return false;
+		}
+		if (this.opt.dragMode) {
+			this.items[this.page - 2].classList.add('transition');
+			this.items[this.page - 1].classList.add('transition');
 		}
 		this.direction = 'prev';
 		methods.slideScroll.call(this, this.page - 2);
